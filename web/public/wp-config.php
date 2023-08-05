74794f825de135d59ba166043d70662923ed80f7
87oui <hello@87oui.com>
2023-08-05 15:59:38 +0900

    お問い合わせ作成

--- web/public/wp-config.php
+++ web/public/wp-config.php
@@ -26,14 +26,14 @@ define( 'DB_COLLATE', '' );
  *
  * @link https://api.wordpress.org/secret-key/1.1/salt/
  */
-define('AUTH_KEY',         '4}z7,T7zHHgoh>$l+w-kA=$`/[vUq.TO(z|&F^8-|+OS%c<kmRZVEfdV@:CYk`mV');
-define('SECURE_AUTH_KEY',  'UOa@)[PNR G?9Sm<DNuhl| Pm)b|2D$O1ysTGA+rpsn+i3Kcw+]*o4@H-U:@c<IR');
-define('LOGGED_IN_KEY',    'PCgw=e?eYLuKn!;>N!gM2m7b=|mP><&`.{!!cv~[uDPM+7z-As##MW}O+Z#e({gG');
-define('NONCE_KEY',        '_Q[Uz[VQ|+@+kGV1mClHXrs2,nKOn1*}g;k(jQN[SnFbEc!T$.sgS9{$z.}b*|;i');
-define('AUTH_SALT',        'tT+^E2;DvoB~F32AT-}|j-COzr@f&TjS~6+9.u^|&D4K/@l-:fJvCL{z+Py.F_Qm');
-define('SECURE_AUTH_SALT', '*fO<$[OLq.Lj[V&DtH Ldx4tp8K_W}tZD-U%}Sm-=&qS64]+@OYSkYaA}1N9n8/k');
-define('LOGGED_IN_SALT',   '7Qg4[~%?5h-3 :J]=ih$PHd+GYw:rwj*~g|b<vvUoBv_dhYL9Fa}`sVhUtT9`O6N');
-define('NONCE_SALT',       'o`!J019uu; f9L^bX8SxXfu|P2pb?,RgWiGJ,@:KKYwdy*h?K-8apAWM~#cHco[f');
+define( 'AUTH_KEY', '4}z7,T7zHHgoh>$l+w-kA=$`/[vUq.TO(z|&F^8-|+OS%c<kmRZVEfdV@:CYk`mV' );
+define( 'SECURE_AUTH_KEY', 'UOa@)[PNR G?9Sm<DNuhl| Pm)b|2D$O1ysTGA+rpsn+i3Kcw+]*o4@H-U:@c<IR' );
+define( 'LOGGED_IN_KEY', 'PCgw=e?eYLuKn!;>N!gM2m7b=|mP><&`.{!!cv~[uDPM+7z-As##MW}O+Z#e({gG' );
+define( 'NONCE_KEY', '_Q[Uz[VQ|+@+kGV1mClHXrs2,nKOn1*}g;k(jQN[SnFbEc!T$.sgS9{$z.}b*|;i' );
+define( 'AUTH_SALT', 'tT+^E2;DvoB~F32AT-}|j-COzr@f&TjS~6+9.u^|&D4K/@l-:fJvCL{z+Py.F_Qm' );
+define( 'SECURE_AUTH_SALT', '*fO<$[OLq.Lj[V&DtH Ldx4tp8K_W}tZD-U%}Sm-=&qS64]+@OYSkYaA}1N9n8/k' );
+define( 'LOGGED_IN_SALT', '7Qg4[~%?5h-3 :J]=ih$PHd+GYw:rwj*~g|b<vvUoBv_dhYL9Fa}`sVhUtT9`O6N' );
+define( 'NONCE_SALT', 'o`!J019uu; f9L^bX8SxXfu|P2pb?,RgWiGJ,@:KKYwdy*h?K-8apAWM~#cHco[f' );
 
 /**
  * Database Table prefix
@@ -52,6 +52,17 @@ define( 'WP_DEBUG', getenv( 'WP_ENV' ) === 'development' );
 * Revision
 */
 define( 'WP_POST_REVISIONS', 3 );
+
+// テスト環境用mailhog設定
+if ( WP_DEBUG ) {
+	define( 'WPMS_ON', true );
+	define( 'WPMS_SMTP_HOST', 'mailhog' ); // The SMTP mail host.
+	define( 'WPMS_SMTP_PORT', 1025 ); // The SMTP server port number.
+	define( 'WPMS_SSL', '' ); // Possible values '', 'ssl', 'tls' - note TLS is not STARTTLS.
+	define( 'WPMS_SMTP_AUTH', false ); // True turns it on, false turns it off.
+	define( 'WPMS_SMTP_AUTOTLS', false ); // True turns it on, false turns it off.
+	define( 'WPMS_MAILER', 'smtp' );
+}
 
 /**
  * Absolute path to the WordPress directory.
